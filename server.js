8ec5a3c2c337a44ad2f13fa964692f7fd57bcd55
sercanda <sercandag52@gmail.com>
2026-02-12 01:10:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const next = require("next");
+const http = require("http");
 
+const port = process.env.PORT || 3000;
+const hostname = "0.0.0.0";
+
+const app = next({ dev: false, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  http
+    .createServer((req, res) => {
+      handle(req, res);
+    })
+    .listen(port, hostname, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
