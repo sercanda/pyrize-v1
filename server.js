937dabfe50327992aa784b812fc08b2c73d10e27
sercanda <sercandag52@gmail.com>
2026-02-12 00:50:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,1 @@
-// server.js (repo root)
 
-process.env.HOSTNAME = "0.0.0.0";
-process.env.HOST = "0.0.0.0";
-process.env.PORT = process.env.PORT || "3000";
-
-require("./.next/standalone/server.js");
