4c51a3b1f240cccee3760bf70834ce422a7db779
sercanda <sercandag52@gmail.com>
2026-02-19 16:17:13 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "i.ibb.co", pathname: "/**" },
+      { protocol: "https", hostname: "picsum.photos", pathname: "/**" },
+      { protocol: "https", hostname: "zocpqnvxdgjnemwwtguj.supabase.co", pathname: "/**" },
+      { protocol: "https", hostname: "zocpqnvxdgjnemwwtguj.supabase.in", pathname: "/**" },
+      { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+      { protocol: "https", hostname: "dummyimage.com", pathname: "/**" },
+    ],
+  },
+
+  experimental: {
+    optimizePackageImports: ["lucide-react"],
+  },
 };
 
 module.exports = nextConfig;
